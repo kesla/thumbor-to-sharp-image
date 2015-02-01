af75dee5ec2170c02aed66e5120d000e22987954
David Björklund <david.bjorklund@gmail.com>
2015-02-01 02:47:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var parseDecrypted = require('thumbor-url').parseDecrypted;
+var Sharp = require('sharp');
+
+module.exports = function (url) {
+  var image = Sharp();
+  var object = parseDecrypted(url);
+
+  if (object && isNumbers(object.crop)) {
+    image = image.extract(
+      object.crop.top,
+      object.crop.left,
+      (object.crop.right - object.crop.left),
+      (object.crop.bottom - object.crop.top)
+    );
+  }
+
+  return image;
+}
+
+function isNumbers(object) {
+  return Object.keys(object).every(function(key) {
+    return typeof object[key] === 'number';
+  });
+}
